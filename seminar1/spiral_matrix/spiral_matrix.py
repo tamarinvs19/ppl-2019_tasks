98235560890bd3e1cfedf24f70615358bfd02ecb
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2021-09-06 20:56:08 +0300

    upd

--- seminar1/spiral_matrix/spiral_matrix.py
+++ seminar1/spiral_matrix/spiral_matrix.py
@@ -7,3 +7,6 @@ def spiral_matrix(n):
          + [[n ** 2 + j - 4*(n - 1) + 1] + matrix[j] + [n**2 - n - j] for j in range(n-2)] \
          + [[n ** 2                      - 3*(n - 1) +  i             for i in range(n)]]
     return new_matrix
+
+def print_spiral_matrix(n):
+    print('\n'.join([' '.join(map(lambda x: str(n**2-x+1).ljust(len(str(n**2)), ' '), line)) for line in spiral_matrix(n)]))
